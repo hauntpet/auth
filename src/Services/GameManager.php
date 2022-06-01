5fb83696549f9b928a49d75a195d3aa8a99c31fc
Paige Jones <hello@paigejones.me>
2022-06-01 09:20:10 +0100

    feat: add game manager

---
+++ src/Services/GameManager.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace HauntPet\Auth\Services;
+
+use HauntPet\Auth\Facades\HauntID;
+
+class GameManager
+{
+    /**
+     * The session key to use.
+     * @var string
+     */
+    private string $gameKey = 'game';
+
+    public function getGame()
+    {
+        return session($this->gameKey);
+    }
+
+    public function hasGame(): bool
+    {
+        return session()->has($this->gameKey);
+    }
+
+    public function setGame(): void
+    {
+        $response = HauntID::game();
+
+        if ($response->ok()) {
+            session([$this->gameKey => $response->json()]);
+        }
+    }
+}
